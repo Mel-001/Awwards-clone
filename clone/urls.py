f363f46bc5f78c7d34acdb747638abe6629b6d18
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-04-10 23:44:51 +0300

    update project urls

--- clone/urls.py
+++ clone/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('camden.urls')),
+    path('', include('Awwards.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 
 ]
